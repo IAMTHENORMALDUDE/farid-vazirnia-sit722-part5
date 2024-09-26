2d112aaafbc21e46d8ddf5c05a82923f235cb934
Farid <91742534+IAMTHENORMALDUDE@users.noreply.github.com>
2024-09-26 23:05:02 +1000

    Initial commit

---
+++ scripts/push-image.sh
@@ -0,0 +1,21 @@
+#
+# Publishes a Docker image.
+#
+# Environment variables:
+#
+#   CONTAINER_REGISTRY - The hostname of your container registry.
+#   REGISTRY_UN - User name for your container registry.
+#   REGISTRY_PW - Password for your container registry.
+#   VERSION - The version number to tag the images with.
+#
+# Usage:
+#
+#       ./scripts/push-image.sh
+#
+
+set -u # or set -o nounset
+: "$CONTAINER_REGISTRY"
+: "$VERSION"
+
+docker push $CONTAINER_REGISTRY/book-catalog:$VERSION
+docker push $CONTAINER_REGISTRY/inventory-management:$VERSION
